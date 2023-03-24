2657d617b812ae497fc2323fc3c3bd0fdcf92026
AceXion <acemalicay19@gmail.com>
2023-03-24 14:24:31 +0800

    text

--- src/components/ProjectData.jsx
+++ src/components/ProjectData.jsx
@@ -12,14 +12,14 @@ const ProjectData = () => {
   const btnclassN = " flex mx-[2rem] mt-12 px-[2rem] py-[1rem] bg-[#00D8FF] font-boldPoppins border-[#0E8388] border-[3px]  text-[#2C3333] rounded-xl"
   return (
     <div id="project" className="bg-[#2E4F4F] ">
-      <div className="container m-auto lg:pb-[10rem] pb-[5rem] px-[1rem] ">
+      <div className="container m-auto lg:pb-[10rem] pb-[5rem] px-[1rem] text-white ">
         <h1 className="text-[#00D8FF] text-center mb-20 pt-20 md:text-[4rem] text-[2rem] font-boldPoppins md:text-left md:mx-[6rem] u-header relative" data-aos="fade-right">
           Project{" "}
           <span className="hover:opacity-100 opacity-5 transition-all duration-200 ease-in-out">
             <Link to="project" duration={500} smooth={true}> # </Link>
           </span>
         </h1>
-          <p className="text-center lg:py-10 max-w-[60rem] mx-auto mb-10 text-white"  data-aos="fade-up">
+          <p className="text-center lg:py-10 max-w-[60rem] mx-auto mb-10 "  data-aos="fade-up">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
             sequi aliquid possimus illo, hic alias, debitis nihil laudantium
             sunt maiores praesentium sapiente natus, provident ipsam. Voluptatem
